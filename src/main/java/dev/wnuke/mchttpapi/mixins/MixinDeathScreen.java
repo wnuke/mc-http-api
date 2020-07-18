558f1d28bede2d4edcbe3863be6f8ef1fd094511
wnuke <wnuke@wnuke.dev>
2020-07-18 19:04:30 +0200

    fix mixin error

--- src/main/java/dev/wnuke/mchttpapi/mixins/MixinDeathScreen.java
+++ src/main/java/dev/wnuke/mchttpapi/mixins/MixinDeathScreen.java
@@ -3,6 +3,7 @@ package dev.wnuke.mchttpapi.mixins;
 import dev.wnuke.mchttpapi.HeadlessAPI;
 import dev.wnuke.mchttpapi.utils.MinecraftCompatLayer;
 import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DeathScreen.class)
 public class MixinDeathScreen {
-    @Inject(method = "<init>", at = @At("HEAD"))
-    public void init(CallbackInfo ci) {
+    @Inject(method = "render", at = @At("HEAD"))
+    public void init(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (MinecraftCompatLayer.getMinecraft().player != null) {
             HeadlessAPI.compatLayer.respawn();
         }
