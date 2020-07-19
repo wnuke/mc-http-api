3cdbed20af2cd93c4208cb8132aa4aa18b58ab4a
wnuke <wnuke@wnuke.dev>
2020-07-19 13:58:40 +0200

    add jvm option to specify port

--- src/main/java/dev/wnuke/mchttpapi/server/HTTPAPIServer.java
+++ src/main/java/dev/wnuke/mchttpapi/server/HTTPAPIServer.java
@@ -18,7 +18,12 @@ public class HTTPAPIServer {
     public static final Gson gson = new GsonBuilder().serializeNulls().create();
 
     public static void httpServer(MinecraftCompatLayer compatLayer) throws IOException {
-        int serverPort = 8000;
+        int serverPort;
+        try {
+            serverPort = Integer.parseInt(System.getProperty("httpapiserverport"));
+        } catch (NumberFormatException e) {
+            serverPort = 8000;
+        }
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         new JsonGETEndpoint(server, "/chat") {
             @Override
