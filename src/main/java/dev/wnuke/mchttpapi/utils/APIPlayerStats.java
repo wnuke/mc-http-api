e4d2a4f9960f07df2bb367aa37f531754c4bb314
wnuke <wnuke@wnuke.dev>
2020-07-27 17:24:04 +0200

    change player stat template

--- src/main/java/dev/wnuke/mchttpapi/utils/APIPlayerStats.java
+++ src/main/java/dev/wnuke/mchttpapi/utils/APIPlayerStats.java
@@ -3,30 +3,20 @@ package dev.wnuke.mchttpapi.utils;
 import com.google.gson.annotations.SerializedName;
 
 public class APIPlayerStats {
-    @SerializedName("Username")
     public String name;
-    @SerializedName("UUID")
     public String uuid;
-    @SerializedName("Player")
     public PlayerInfo player;
-    @SerializedName("Coordinates")
     public Position coordinates;
 
     public static class PlayerInfo {
-        @SerializedName("Health")
         public float health;
-        @SerializedName("Hunger")
         public float hunger;
-        @SerializedName("Saturation")
         public float saturation;
     }
 
     public static class Position {
-        @SerializedName("X")
         public double x;
-        @SerializedName("Y")
         public double y;
-        @SerializedName("Z")
         public double z;
 
         public Position(double x, double y, double z) {
